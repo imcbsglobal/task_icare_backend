6f65182f6180854b0155bb18834fde6fa4fd3997
Rashmitha nr <rashmithanrrashmithanr@gmail.com>
2025-11-20 12:55:27 +0530

    update

--- icare_backend/icare_backend/settings.py
+++ icare_backend/icare_backend/settings.py
@@ -140,7 +140,7 @@ REST_FRAMEWORK = {
 # -----------------------------
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://localhost:5173",  # If using Vite
+    "http://localhost:5173",  
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
 ]
